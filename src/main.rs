20c8bed61b35c86eae8cec3d03b6468b4d9da64d
Martin Disch <martindisch@gmail.com>
2019-07-06 11:13:46 +0200

    Add prime example with ITM output

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ extern crate panic_halt;
 
 use cortex_m::iprintln;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
 use f3::{
     hal::{delay::Delay, prelude::*, stm32f30x},
     led::Leds,
@@ -28,9 +27,13 @@ fn main() -> ! {
     let mut leds = Leds::new(gpioe);
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    hprintln!("Hello, world!").unwrap();
-    iprintln!(stim, "Hello, world!");
+    let primes = Primes::new(2);
+    for (i, n) in primes.enumerate() {
+        iprintln!(stim, "{:>6}: {}", i, n);
+        // TODO: Advance LED here
+    }
 
+    // Unreachable
     let n = leds.len();
     loop {
         for curr in 0..n {
@@ -41,4 +44,39 @@ fn main() -> ! {
             delay.delay_ms(100_u8);
         }
     }
+}
+
+struct Primes {
+    curr: u32,
+}
+
+impl Primes {
+    fn new(start: u32) -> Self {
+        Self { curr: start - 1 }
+    }
+}
+
+impl Iterator for Primes {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        loop {
+            self.curr += 1;
+            if let Some(n) = check_prime(self.curr) {
+                break Some(n);
+            }
+        }
+    }
+}
+
+fn check_prime(n: u32) -> Option<u32> {
+    let middle = (n as f32 / 2f32) as u32 + 1;
+
+    for i in 2..middle {
+        if n % i == 0 {
+            return None;
+        }
+    }
+
+    Some(n)
 }
