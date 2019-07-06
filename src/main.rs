3981691a9582fb1ce732e1afe589ec0fb18f9a78
Martin Disch <martindisch@gmail.com>
2019-07-06 11:06:01 +0200

    Get LEDs running

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
+use cortex_m::iprintln;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use cortex_m::{iprintln, Peripherals};
+use f3::{
+    hal::{delay::Delay, prelude::*, stm32f30x},
+    led::Leds,
+};
 
 #[entry]
 fn main() -> ! {
-    let mut p = Peripherals::take().unwrap();
-    let stim = &mut p.ITM.stim[0];
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let stim = &mut cp.ITM.stim[0];
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    // clock configuration using the default settings (all clocks run at 8 MHz)
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut leds = Leds::new(gpioe);
+    let mut delay = Delay::new(cp.SYST, clocks);
 
     hprintln!("Hello, world!").unwrap();
     iprintln!(stim, "Hello, world!");
 
-    loop {}
+    let n = leds.len();
+    loop {
+        for curr in 0..n {
+            let next = (curr + 1) % n;
+            leds[curr].off();
+            leds[next].on();
+
+            delay.delay_ms(100_u8);
+        }
+    }
 }
